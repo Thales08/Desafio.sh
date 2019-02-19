d4cdde058feb8d861091dffbe9f4d9498df5a34f
Thales08 <43183832+Thales08@users.noreply.github.com>
2019-02-19 16:23:56 -0300

    Add files via upload

---
+++ thales1.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+HORA=$(date +%H) 
+HORAM=$(date +%M)
+HORAS=$(date +%S) 
+DIASEM=$(date +%a)
+DIA=$(date +%d)
+MES=$(date +%b)
+ANO=$(date +%y)
+
+echo "Chora agora, ri depois, mais um dia de luta... BOM DIA QUEBRADA $DIA/$MES/$ANO"
+echo "$HORA:$HORAM:$HORAS"
+
+if [ -e "/tmp" ];
+then
+echo " o diretório  existe" 
+else
+echo " o diretório não existe"
+fi
+
+if [ -e "/tmp/thales" ]; 
+then
+echo "o diretório foi criado"
+else 
+echo "o diretório não foi criado"
+mkdir /tmp/thales
+fi
+
+if [ -e "/tmp/thales/thales.txt" ]; 
+then
+echo "o diretório foi criado"
+else 
+echo "o diretório não foi criado"
+touch /tmp/thales/thales.txt
+fi
+
+
+
+
